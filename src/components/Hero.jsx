02b5b4c50411a9aa47d477c04dd0cb0529750da8
Aadil Saudagar <aadilsaudagar24@gmail.com>
2022-07-28 23:59:46 +0530

    Done with hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
-        {/* OverLay */}
-        <div>
-          <h1>
-            The <span>Best</span>
+    <div className="max-w-[1640px] mx-auto p-4 ">
+      {" "}
+      {/* Since for big monitors we do not want the componenet to strect the whole width of screen */}
+      <div className="max-h-[500px] relative ">
+        {/* OverLay heading part */}
+        <div className="absolute w-full h-full max-h-[500px] text-gray-200 bg-black/40 flex flex-col justify-center items-start ">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            The <span className="text-orange-500">Best</span>
           </h1>
-          <h1>
-            Foods <span>Delivered</span>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            <span className="text-orange-500">Foods</span> Delivered{" "}
           </h1>
         </div>
+        <img
+          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg"
+          alt="Food Image"
+          className="w-full h-full object-cover max-h-[500px]"
+        />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = (props) => {
             }}
           />
         </div>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+        <h1 className="text-2xl sm:text-2xl lg:text-3xl px-2">
           Best <span className="font-bold">Eats</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
